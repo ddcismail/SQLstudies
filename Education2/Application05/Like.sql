319ab77e71fdf65d5b8a9c7b4aa324b51238d0ac
ismaildedec <ismaildedec@gmail.com>
2022-06-24 15:34:27 +0300

    Lıke komutu eklendi.

---
+++ Education2/Application05/Like.sql
@@ -0,0 +1,5 @@
+select	* from Products where ProductName like 'ch%' --ch ile baþlayanlarý getirecek (alýþveriþ sitesinde ürün ismi tam hatýrlanmadýgýnda)
+
+select	* from Products where ProductName like '%a' --Baþý onemli deðil sonu a ile biten.
+
+select	* from Products where ProductName like '%ch%' --içinde ch bulunan
