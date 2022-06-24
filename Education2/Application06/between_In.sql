6afb6dcad4ab2e6b44ece6067a69e71a909fad92
ismaildedec <ismaildedec@gmail.com>
2022-06-24 15:35:55 +0300

    Between Ve In Komutu Eklendi.

---
+++ Education2/Application06/between_In.sql
@@ -0,0 +1,11 @@
+select	* from Products where ProductName like 'ch%' --ch ile baþlayanlarý getirecek (alýþveriþ sitesinde ürün ismi tam hatýrlanmadýgýnda)
+
+select	* from Products where ProductName like '%a' --Baþý onemli deðil sonu a ile biten.
+
+select	* from Products where ProductName like '%ch%' --içinde ch bulunan
+
+select	* from Products where UnitPrice between 10 and 50 --10 ile 50 arasýnda olan ürünler
+
+select	* from Products where UnitPrice between 10 and 50 order by UnitPrice--10 ile 50 arasýnda olan ürünleri sýralar
+
+select	* from Products where CategoryID in (1,2) --CategoryID 1 veya 2 olanlar
