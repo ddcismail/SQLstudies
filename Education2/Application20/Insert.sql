57430e1c0cfe20e9d136f202650b86440e09030b
ismaildedec <ismaildedec@gmail.com>
2022-07-27 21:13:30 +0300

    Insert Çalışması Eklendi

---
+++ Education2/Application20/Insert.sql
@@ -0,0 +1,7 @@
+--Insert
+
+insert into Categories (CategoryName,Description)    --Hangi Alanlara Kayit Eklemek istiyor isek bunu kullanýyoruz
+values ('Test Category' , 'Test Category Description')
+
+insert into [Order Details] --Bu ornekte kolonlari vermeden yapiyoruz.
+values(10248,12,12,10,0)    --eger kollanlari vermezsek tüm kolanlarin degerlerini eklememiz gerekir.
